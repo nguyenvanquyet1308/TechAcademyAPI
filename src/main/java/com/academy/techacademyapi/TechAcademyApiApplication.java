78c20c0ae1f9255359412e64d74ce1ee0e41930e
nguyen van quyet <nam.d02th@gmail.com>
2025-03-27 08:54:55 +0700

    first commit

---
+++ src/main/java/com/academy/techacademyapi/TechAcademyApiApplication.java
@@ -0,0 +1,13 @@
+package com.academy.techacademyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechAcademyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechAcademyApiApplication.class, args);
+	}
+
+}
